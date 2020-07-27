d6f39379c7a8f9149df4631033a2966b627939d1
Ebihara Yusuke <ebiyuu1121@gmail.com>
2020-07-27 12:46:12 +0900

    クリア判定を実装

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -17,12 +17,15 @@ public class GameManager : MonoBehaviour
 	[SerializeField] Color firstPlayerColor;
 	[SerializeField] Color secondPlayerColor;
 	[SerializeField] Text turnText;
+	[SerializeField] Text clearText;
+	[SerializeField] Image clearTextBackground;
 
 	int frameSize = 100;
 	GameObject[,] frameObjects;
 	GameObject[,] circleObjects;
 	int[,] circleStatus;
 	int activePlayer = 0;
+	bool finished = false;
 
 	// Start is called before the first frame update
 	void Start()
@@ -38,6 +41,7 @@ public class GameManager : MonoBehaviour
 
 	public void addCircle(int col)
 	{
+		if(finished) return;
 		for(int i = 0; i < rows; i++){
 			if(circleStatus[i, col] == 0){
 				circleStatus[i, col] = activePlayer;
@@ -46,6 +50,21 @@ public class GameManager : MonoBehaviour
 					activePlayer = 2;
 				}else{
 					activePlayer = 1;
+				}
+				int winner = clearCheck();
+				if(winner != 0)
+				{
+					if(winner == 1)
+					{
+						clearText.text = "Red win";
+						clearText.color = Color.red;
+					}else{
+						clearText.text = "Yellow win";
+						clearText.color = Color.yellow;
+					}
+					finished = true;
+					clearText.enabled = true;
+					clearTextBackground.enabled = true;
 				}
 				displayTurn();
 				break;
@@ -71,6 +90,97 @@ public class GameManager : MonoBehaviour
 		obj.targetY = (row - rows / 2.0f + 0.5f) * frameSize;
 
 		circleObjects[row, col] = circle;
+	}
+
+	private int clearCheck()
+	{
+		if(rows < 4 || cols < 4) return 0;
+
+
+		int[][] wonPosition = new int[4][];
+		Debug.Log(wonPosition);
+
+		// check horizontal
+		for(int i = 0; i < rows; i++)
+		{
+			for(int j = 0; j < cols - 3; j++)
+			{
+				int winner = circleStatus[i, j];
+				if(winner == 0) continue;
+				bool won = true;
+				for(int k = 1; k < 4; k++)
+				{
+					if(circleStatus[i, j + k] != winner)
+					{
+						won = false;
+					}
+				}
+				if(won) return winner;
+			}
+		}
+
+		// check vertical
+		for(int i = 0; i < rows - 3; i++)
+		{
+			for(int j = 0; j < cols; j++)
+			{
+				int winner = circleStatus[i, j];
+				if(winner == 0) continue;
+				Debug.Log(winner);
+				bool won = true;
+				for(int k = 1; k < 4; k++)
+				{
+					if(circleStatus[i + k, j] != winner)
+					{
+						won = false;
+					}
+				}
+				if(won) return winner;
+			}
+		}
+
+		// check diagonal "/"
+		for(int i = 0; i < rows - 3; i++)
+		{
+			for(int j = 0; j < cols - 3; j++)
+			{
+				int winner = circleStatus[i, j];
+				if(winner == 0) continue;
+				Debug.Log(winner);
+				bool won = true;
+				for(int k = 1; k < 4; k++)
+				{
+					if(circleStatus[i + k, j + k] != winner)
+					{
+						won = false;
+					}
+				}
+				if(won) return winner;
+			}
+		}
+
+		// check diagonal "\"
+		for(int i = 0; i < rows - 3; i++)
+		{
+			for(int j = 3; j < cols; j++)
+			{
+				int winner = circleStatus[i, j];
+				if(winner == 0) continue;
+				Debug.Log(winner);
+				bool won = true;
+				for(int k = 1; k < 4; k++)
+				{
+					if(circleStatus[i + k, j - k] != winner)
+					{
+						won = false;
+					}
+				}
+				if(won) return winner;
+			}
+		}
+
+
+		return 0;
 	}
 
 
@@ -114,6 +224,11 @@ public class GameManager : MonoBehaviour
 
 		activePlayer = 1;
 		displayTurn();
+
+		finished = false;
+
+		clearText.enabled = false;
+		clearTextBackground.enabled = false;
 	}
 
 	private void displayTurn(){
